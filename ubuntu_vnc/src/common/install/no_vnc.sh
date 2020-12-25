e194f6e4a788fae6c77433b0061e053232d9ffcf
lianshufeng <251708339@qq.com>
2020-12-25 17:16:33 +0800

    fix bug

--- ubuntu_vnc/src/common/install/no_vnc.sh
+++ ubuntu_vnc/src/common/install/no_vnc.sh
@@ -11,3 +11,10 @@ wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --s
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
+
+
+# fix bug : python version > 2
+pythonPath=/usr/bin/python
+if [ ! -f "$pythonPath" ]; then
+	ln -s /usr/bin/python2 /usr/bin/python
+fi
